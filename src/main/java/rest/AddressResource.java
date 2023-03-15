00e9b100fc1f5446b42b40294dca487923a4e320
Pelle <cph-pd94@cphbusiness.dk>
2023-03-15 13:09:17 +0100

    Tilføjet exception or error handling i AddressResource klassen, og lavet en test klasse af samme klasse

--- src/main/java/rest/AddressResource.java
+++ src/main/java/rest/AddressResource.java
@@ -5,68 +5,105 @@ import com.google.gson.GsonBuilder;
 import dtos.AddressDTO;
 import dtos.CityInfoDTO;
 import facades.AddressFacade;
-import facades.CityInfoFacade;
 import utils.EMF_Creator;
 
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-//Todo Remove or change relevant parts before ACTUAL use
 @Path("address")
 public class AddressResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
-       
     private static final AddressFacade FACADE =  AddressFacade.getAddressFacade(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-            
+    private static final Logger LOGGER = Logger.getLogger(AddressResource.class.getName());
+
+
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public Response getAll() {
-        return Response.ok().entity(GSON.toJson(FACADE.getAllAddresses())).build();
+        try {
+            return Response.ok().entity(GSON.toJson(FACADE.getAllAddresses())).build();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "An error occurred while getting all addresses", e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("An error occurred while getting all addresses").build();
+        }
     }
 
     @GET
     @Path("/{adress}")
     @Produces({MediaType.APPLICATION_JSON})
     public Response getCityInfoByZipCode(@PathParam("adress") String adress) {
-        CityInfoDTO cityInfoDTO = FACADE.getAddressByName(adress);
-        return Response.ok().entity(GSON.toJson(cityInfoDTO)).build();
+        try {
+            CityInfoDTO cityInfoDTO = FACADE.getAddressByName(adress);
+            if (cityInfoDTO == null) {
+                return Response.status(Response.Status.NOT_FOUND).entity("Address not found").build();
+            }
+            return Response.ok().entity(GSON.toJson(cityInfoDTO)).build();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "An error occurred while getting city info by address", e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("An error occurred while getting city info by address").build();
+        }
     }
 
     @GET
     @Path("/addressByZipCode/{CityInfo_zipCode}")
     @Produces({MediaType.APPLICATION_JSON})
     public Response getCityInfoByCity(@PathParam("CityInfo_zipCode") int zipCode) {
-        CityInfoDTO cityInfoDTO = FACADE.getAddressByZipCode(zipCode);
-        return Response.ok().entity(GSON.toJson(cityInfoDTO)).build();
+        try {
+            CityInfoDTO cityInfoDTO = FACADE.getAddressByZipCode(zipCode);
+            if (cityInfoDTO == null) {
+                return Response.status(Response.Status.NOT_FOUND).entity("City info not found").build();
+            }
+            return Response.ok().entity(GSON.toJson(cityInfoDTO)).build();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "An error occurred while getting city info by zip code", e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("An error occurred while getting city info by zip code").build();
+        }
     }
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String editAddress(String addressJson) {
-        AddressDTO addressDTO = GSON.fromJson(addressJson, AddressDTO.class);
-        AddressDTO updatedAddress = FACADE.editAddress(addressDTO);
-        return GSON.toJson(updatedAddress);
+    public Response editAddress(String addressJson) {
+        try {
+            AddressDTO addressDTO = GSON.fromJson(addressJson, AddressDTO.class);
+            AddressDTO updatedAddress = FACADE.editAddress(addressDTO);
+            if (updatedAddress == null) {
+                return Response.status(Response.Status.NOT_FOUND).entity("Address not found.").build();
+            }
+            return Response.ok().entity(GSON.toJson(updatedAddress)).build();
+        } catch (Exception e) {
+            return Response.serverError().entity("An error occurred while editing address.").build();
+        }
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String createAddress(String addressJson) {
-        AddressDTO addressDTO = GSON.fromJson(addressJson, AddressDTO.class);
-        AddressDTO newAddress = FACADE.addAddress(addressDTO);
-        return GSON.toJson(newAddress);
+    public Response createAddress(String addressJson) {
+        try {
+            AddressDTO addressDTO = GSON.fromJson(addressJson, AddressDTO.class);
+            AddressDTO newAddress = FACADE.addAddress(addressDTO);
+            return Response.ok().entity(GSON.toJson(newAddress)).build();
+        } catch (Exception e) {
+            return Response.serverError().entity("An error occurred while creating address.").build();
+        }
     }
 
     @DELETE
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String deleteAddress(@PathParam("id") String id) {
-        AddressDTO deletedAddress = FACADE.deleteAddress(id);
-        return GSON.toJson(deletedAddress);
+    public Response deleteAddress(@PathParam("id") String id) {
+        try {
+            AddressDTO deletedAddress = FACADE.deleteAddress(id);
+            return Response.ok().entity(GSON.toJson(deletedAddress)).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(GSON.toJson(e.getMessage())).build();
+        }
     }
 }
---
+++ src/test/java/rest/AddressResourceTest.java
@@ -0,0 +1,33 @@
+package rest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import javax.ws.rs.core.Response;
+
+public class AddressResourceTest {
+
+    @Test
+    public void testGetAllAddressErrorHandling() {
+        AddressResource addressResource = new AddressResource();
+        Response response = addressResource.getAll();
+        assertEquals(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), response.getStatus());
+        assertTrue(response.getEntity().toString().contains("An error occurred while getting all addresses"));
+    }
+
+    @Test
+    public void testGetCityInfoByZipCodeErrorHandling() {
+        AddressResource addressResource = new AddressResource();
+        Response response = addressResource.getCityInfoByZipCode("invalid_address");
+        assertEquals(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), response.getStatus());
+        assertTrue(response.getEntity().toString().contains("An error occurred while getting city info by zipcode"));
+    }
+
+    @Test
+    public void testGetCityInfoByCityErrorHandling() {
+        AddressResource addressResource = new AddressResource();
+        Response response = addressResource.getCityInfoByCity(0);
+        assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatus());
+        assertTrue(response.getEntity().toString().contains("City info not found"));
+    }
+}
